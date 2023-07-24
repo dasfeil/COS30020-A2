dc03b82c1d0b6acf8066bf4ee7361b445c980b55
dasfeil <sfbgamng@gmail.com>
2023-07-24 16:24:36 +0700

    Index page

--- index.php
+++ index.php
@@ -6,18 +6,73 @@
     <meta name="description" content="Web application development" />
     <meta name="keywords" content="PHP" />
     <meta name="author" content="Nguyen The Vinh" />
-    <link rel="stylesheet" href="style.css" />
     <title>Assignment 1</title>
+    <link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
     <nav>
         <ul>
-            <li class="<?php echo basename(htmlspecialchars($_SERVER["PHP_SELF"])) == "index.php" ? "liactive" : ""; ?>">
+            <li
+                class="<?php echo basename(htmlspecialchars($_SERVER["PHP_SELF"])) == "index.php" ? "liactive" : ""; ?>">
                 <a href="index.php">Home</a>
             </li>
         </ul>
     </nav>
+    <div class="">
+        <h1>My Friend System Assignment Home Page</h1>
+        <p>Name: Nguyen The Vinh</p>
+        <p>Student ID: 102437961</p>
+        <p>Email: <a href="mailto:vinhntswh00009@fpt.edu.vn">vinhntswh00009@fpt.edu.vn</a></p>
+        <p>
+            I declare that this assignment is my individual work.
+            I have not worked collaboratively nor have I
+            copied from any other student's work or from any other source</a>
+        </p>
+        <div class="">
+            <div><a href="#">Sign-Up</a></div>
+            <div><a href="#">Log-In</a></div>
+            <div><a href="#">About</a></div>
+        </div>
+    </div>
+    <?php
+    require_once("settings.php");
+
+    $conn = new mysqli("feenix-mariadb.swin.edu.au", $username, $password, $dbname);
+    if ($conn->connect_error)
+        die("Unable to connect");
+    try {
+        $query = "CREATE TABLE IF NOT EXISTS `friends` ( `friend_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+        `friend_email` VARCHAR(50) NOT NULL, `password` VARCHAR(20) NOT NULL, `profile_name` VARCHAR(30) NOT NULL,
+        `date_started` DATE NOT NULL, `num_of_friends` INTEGER UNSIGNED);";
+        $conn->query($query);
+        $query = "CREATE TABLE IF NOT EXISTS `myfriends` ( `friend_id1` INT NOT NULL, `friend_id2` INT NOT NULL)";
+        $conn->query($query);
+        $query = "SELECT * FROM `friends` LIMIT 1";
+        $result = $conn->query($query);
+        $nodata = true;
+        if ($result->num_rows > 0) {
+            $nodata = false;
+        }
+        if ($nodata) {
+            foreach ($friendsdata as $data) {
+                $query = "INSERT INTO `friends` (`friend_email`, `password`, `profile_name`, `date_started`, `num_of_friends`) 
+                VALUES ('". $data['friend_email'] . "','" . $data['password'] . "','" . $data['profile_name'] . "','" . 
+                $data['date_started'] . "','" . $data['num_of_friends'] . "');";
+                $conn->query($query);
+            }
+            foreach ($myfriendsdata as $data) {
+                $query = "INSERT INTO `myfriends` (`friend_id1`, `friend_id2`) 
+                VALUES ('". $data['friend_id1'] . "','" . $data['friend_id2'] . "');";
+                $conn->query($query);
+            }
+        }
+        echo "<p>Tables successfully created and populated</p>";
+    } catch (Exception $e) {
+        echo "<p>$e</p>";
+    }
+    $conn->close();
+    ?>
 </body>
 
 </html>
---
+++ settings.php
@@ -0,0 +1,99 @@
+<?php
+$host = "feenix-mariadb.swin.edu.au";
+$username = "s103427961";
+$password = "bruhmoment";
+$dbname = "s103427961_db";
+$friendsdata = array(
+    array(
+        'friend_email' => 'dreamer92@example.com',
+        'password' => 'abcdefg',
+        'profile_name' => 'Dreamer92',
+        'date_started' => '2021-01-01',
+        'num_of_friends' => 3
+    ),
+    array(
+        'friend_email' => 'sunshine23@example.com',
+        'password' => 'hijk123',
+        'profile_name' => 'Sunshine23',
+        'date_started' => '2020-03-15',
+        'num_of_friends' => 4
+    ),
+    array(
+        'friend_email' => 'adventureseeker@example.com',
+        'password' => 'p@ssw0rd',
+        'profile_name' => 'AdventureSeeker',
+        'date_started' => '2022-08-10',
+        'num_of_friends' => 7
+    ),
+    array(
+        'friend_email' => 'musiclover88@example.com',
+        'password' => '987654321',
+        'profile_name' => 'MusicLover88',
+        'date_started' => '2019-05-20',
+        'num_of_friends' => 5
+    ),
+    array(
+        'friend_email' => 'bookworm27@example.com',
+        'password' => 'qwerty',
+        'profile_name' => 'Bookworm27',
+        'date_started' => '2023-02-14',
+        'num_of_friends' => 3
+    ),
+    array(
+        'friend_email' => 'stargazer77@example.com',
+        'password' => 'hello123',
+        'profile_name' => 'StarGazer77',
+        'date_started' => '2022-11-30',
+        'num_of_friends' => 4
+    ),
+    array(
+        'friend_email' => 'wanderlust18@example.com',
+        'password' => 'ilovecats',
+        'profile_name' => 'Wanderlust18',
+        'date_started' => '2020-09-05',
+        'num_of_friends' => 4
+    ),
+    array(
+        'friend_email' => 'fitnessjunkie@example.com',
+        'password' => 'mypassword',
+        'profile_name' => 'FitnessJunkie',
+        'date_started' => '2023-06-10',
+        'num_of_friends' => 5
+    ),
+    array(
+        'friend_email' => 'artenthusiast@example.com',
+        'password' => '12345678',
+        'profile_name' => 'ArtEnthusiast',
+        'date_started' => '2021-04-18',
+        'num_of_friends' => 4
+    ),
+    array(
+        'friend_email' => 'foodie44@example.com',
+        'password' => 'letmein',
+        'profile_name' => 'Foodie44',
+        'date_started' => '2019-12-25',
+        'num_of_friends' => 3
+    )
+);
+$myfriendsdata = array(
+    array('friend_id1' => 0, 'friend_id2' => 1),
+    array('friend_id1' => 0, 'friend_id2' => 2),
+    array('friend_id1' => 0, 'friend_id2' => 3),
+    array('friend_id1' => 1, 'friend_id2' => 2),
+    array('friend_id1' => 1, 'friend_id2' => 8),
+    array('friend_id1' => 2, 'friend_id2' => 3),
+    array('friend_id1' => 2, 'friend_id2' => 9),
+    array('friend_id1' => 4, 'friend_id2' => 5),
+    array('friend_id1' => 4, 'friend_id2' => 7),
+    array('friend_id1' => 4, 'friend_id2' => 8),
+    array('friend_id1' => 5, 'friend_id2' => 6),
+    array('friend_id1' => 5, 'friend_id2' => 3),
+    array('friend_id1' => 6, 'friend_id2' => 7),
+    array('friend_id1' => 6, 'friend_id2' => 8),
+    array('friend_id1' => 7, 'friend_id2' => 5),
+    array('friend_id1' => 7, 'friend_id2' => 2),
+    array('friend_id1' => 8, 'friend_id2' => 2),
+    array('friend_id1' => 9, 'friend_id2' => 3),
+    array('friend_id1' => 9, 'friend_id2' => 1),
+);
+?>
